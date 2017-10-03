de94478833ee4a8bb795ccb86a8a57d6696f529d
= <agustincc4@gmail.com>
2017-10-03 13:42:49 +0200

    Update

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,7 +1,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var addend1: Double = 0.0;
+    var addend2: Double = 0.0;
 
+    @IBOutlet weak var screen: UILabel!
+    
+    // pagina 57
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        let digito: String! = sender.currentTitle
+        screen.text = screen.text! + digito
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        screen.text = "0";
+    }
+    
+    @IBAction func divide(_ sender: UIButton) {
+        //numberOnScreen = Double(screen.text!)!;
+        screen.text = "";
+    }
+    
+    @IBAction func equal(_ sender: UIButton) {
+        //let numberScreen: Double = Double(screen.text!)!;
+        //let result : Double = numberOnScreen / numberScreen;
+        //screen.text = String(result);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
